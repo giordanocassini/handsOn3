f435d86c45bdc5df5409b2ebb9eeea10eafcb6da
giordanocassini <giordano.cassini@gmail.com>
2022-07-25 22:19:59 -0300

    refactor: portugues to english, prettier fix: add alguma linha faltante

--- src/controller/psicologoController.js
+++ src/controller/psicologoController.js
@@ -1,78 +1,69 @@
 const Psicologos = require("../models/Psicologos");
 
 const psicologoController = {
-    listarPsicologo: async (req, res) =>{
 
-        const listaDePsicologos = await Psicologos.findAll()
+  async getAllPiscologos(req, res) {
+    const picologosList = await Psicologos.findAll();
+    res.json(picologosList);
+  },
 
-        res.json(listaDePsicologos)
-    },
+  async getPiscologo(req, res) {
+    const { id } = req.params;
+    const piscologo = await Psicologos.findOne({
+      where: {
+        id,
+      },
+    });
 
-    listarPsicologoId: async (req, res) =>{
+    res.json(piscologo);
+  },
 
-        const listaDePsicologosId = await Psicologos.findOne({
-            where: { 
-                id,
-            },
-        }    
-        )
+  async createPsicologo(req, res) {
+    const { name, email, password, presentation } = req.body;
 
-        res.json(listaDePsicologosId)
-    },
-        
+    // Cadastrando um novo produto através do metodo .create do sequelize conforme modelo pre definido
+    const newPsicologo = await Psicologos.create({
+      name,
+      email,
+      password,
+      presentation,
+    });
 
-    async cadastrarPsicologo(req, res){
-        const { nome, email, senha, apresentacao } = req.body;
+    console.log(req.body);
+    // Resposta mostrada apos metodo .create
+    res.json(newPsicologo);
+  },
 
-        // Cadastrando um novo produto através do metodo .create do sequelize conforme modelo pre definido 
-        const novoPsicologo = await Psicologos.create({
-            nome,
-            email,
-            senha,
-            apresentacao,
-        });
+  async deletarPsicologo(req, res) {
+    const { id } = req.params;
+    await Psicologos.destroy({
+      where: {
+        id,
+      },
+    });
 
-        console.log(req.body);
-        // Resposta mostrada apos metodo .create
-        res.json(novoPsicologo);
-    },
+    res.json("Psicologo Deletado");
+  },
 
-    async deletarPsicologo(req, res) {
+  async atualizarPsicologo(req, res) {
+    const { id } = req.params;
+    const { name, email, password, presentation } = req.body;
 
-        // desestruturarando o objeto usando o {id} do psicologo a ser deletado
-        const {id} = req.params;
-
-        // Função assincrona, apartir modelo Psicologos, aplica-se o metodo .destroy
-        await Psicologos.destroy({
-        // Criamos a propiedade where que especifica onde, pelo ID, o metodo .destroy será aplicado.
-            where: { 
-                id,
-            },
-        });
-
-        res.json("Psicologo Deletado")
-    },
-
-    async atualizarPsicologo (req, res) {
-        const {id} = req.params;
-        const {nome, email, senha, apresentacao} = req.body;
-
-        const psicologoAtualizado = await Psicologos.update({
-            nome,
-            email,
-            senha,
-            apresentacao,
+    const psicologoAtualizado = await Psicologos.update(
+      {
+        name,
+        email,
+        password,
+        presentation,
+      },
+      {
+        where: {
+          id,
         },
-        {
-            where: {
-                id,
-            },
-        }
-        );
-        res.json("Psicologo Atualizado");        
-    },
-
-    
+      }
+    );
+    res.json("Psicologo Atualizado");
+  },
 };
 
 module.exports = psicologoController;
