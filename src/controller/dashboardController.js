6d070c77539ac29e52b670de921b21a3c4348ed4
VictorNery <victorcndp@gmail.com>
2022-07-29 06:48:27 -0300

    fix: dashboard

--- src/controller/dashboardController.js
+++ src/controller/dashboardController.js
@@ -9,19 +9,18 @@ const dashboard = {
     
     async numeroDePacientes (req, res) {
         const numeroDePacientes = await Pacientes.findAndCountAll();
-        return res.json(`Total de pacientes cadastrados : ${numeroDePacientes.count}`);
+        return res.json(`Total de pacientes cadastrados: ${numeroDePacientes.count}`);
     },
 
     async numeroDeAtendimentos (req, res) {
         const numeroDeAtendimentos = await Atendimentos.findAndCountAll();
-        return res.json(`Total de atendimentos cadastrados : ${numeroDeAtendimentos.count}`);
+        return res.json(`Total de atendimentos cadastrados: ${numeroDeAtendimentos.count}`);
     },
 
     async mediaAtendimentosPsicologos (req, res) {
         const numeroDeAtendimentos = await Atendimentos.findAndCountAll();
         const numeroDePsicologos = await Psicologos.findAndCountAll();
-        return res.json(`A média de atendimentos por psicologos é : 
-        ${numeroDeAtendimentos.count / numeroDePsicologos.count}`);
+        return res.json(`A média de atendimentos por psicologos é: ${numeroDeAtendimentos.count / numeroDePsicologos.count}`);
     }   
     
 };
