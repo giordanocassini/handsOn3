97d6f51328a0667a549de60f2868cf784e1f7930
VictorNery <victorcndp@gmail.com>
2022-07-30 09:19:57 -0300

    fix: dashboard app

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,11 +2,9 @@ const dashboard = require("../controller/dashboardController");
 const express = require("express");
 const router = express.Router();
 
-router
-  .route("/")
-  .get( dashboard.numeroDeAtendimentos )
-  .get( dashboard.numeroDePsicologos )
-  .get( dashboard.numeroDePacientes )
-  .get( dashboard.mediaAtendimentosPsicologos)
+router.route("/atendimentos").get( dashboard.numeroDeAtendimentos )  
+router.route("/psicologos").get( dashboard.numeroDePsicologos )
+router.route("/pacientes").get(dashboard.numeroDePacientes)
+router.route("/mediaAtendimentosPsicologos").get( dashboard.mediaAtendimentosPsicologos)
   
 module.exports = router;
